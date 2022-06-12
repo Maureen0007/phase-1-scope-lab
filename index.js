ac1e0a67275b4cb229ae2af68a31a61326520149
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-06-12 14:59:08 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+var customerName =  'bob';
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+    
+}
+
+var bestCustomer;
+ function setBestCustomer() {
+     bestCustomer= 'not bob';
+
+     return bestCustomer;
+     
+ }
+
+ function overwriteBestCustomer() {
+     bestCustomer = 'maybe bob';
+
+     return bestCustomer;
+     
+ }
+
+ const leastFavoriteCustomer = 'bob';
+ function changeLeastFavoriteCustomer() {
+     leastFavoriteCustomer = 'ann';
+
+     return leastFavoriteCustomer;
+ }
